e4028a9755ecc82ad9d9c03546ccd84a4e625efe
Gojko Adzic <gojko@gojko.com>
2017-12-19 00:26:51 +0100

    merge config and components to make syntax simpler

--- example/receiver-impl.js
+++ example/receiver-impl.js
@@ -1,7 +1,8 @@
 'use strict';
 // this module demonstrates how you can use functions as components. check out sender-impl for how to use classes
-module.exports = function ReceiverImpl(config /*, components */) { //could ask for additional components, but does not need it
+module.exports = function ReceiverImpl(config) {
 	this.receive = function (message) {
+		// ask for components or parameters by name
 		console.log(config.name + ' received ' + message);
 	};
 };
